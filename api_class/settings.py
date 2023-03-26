6ec795ccd069654006becf7d5359de2199c0c804
Kai Pho <kgp0@yahoo.com>
2023-03-25 21:05:32 -0700

    remove secret key

--- api_class/settings.py
+++ api_class/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jwgy8@4v2mg*+uy18*a=era*fsz7@(r=g7o!(0$lsfr*e)4@(c'
+SECRET_KEY = parser["BASE"]["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
